e000c6956c2f98881bde763cc851caee1299b06e
MRAMIRE <55102423+MRAMIRE@users.noreply.github.com>
2019-12-29 12:43:03 -0500

    Update DataSet.py
    Data set version con re escalamiento de imagenes

--- md_tesis/DataSet.py
+++ md_tesis/DataSet.py
@@ -1,24 +1,37 @@
-from torch.utils.data import Dataset, DataLoader
-import torch
-import os
-import pandas as pd
-import numpy as np
-from torchvision import transforms, utils
-
 class CloudDataSet(Dataset):
-    def __init__(self, cvs_file, root_dir, trnasform=None):
-        super().__init__()
-        self.set_train = pd.read_csv(csv_file)
+  
+
+  def __init__(self, csv_file, root_dir, transform=None):
+        self.set_data = pd.read_csv(csv_file)
         self.root_dir =  root_dir
         self.transform = transform
 
-def __len__(self):
-    return len(self.set_train)
+  def __len__(self):
+    return len(self.set_data)
 
-def __getitem__(self, idx):
+  def __getitem__(self, idx):
     if torch.is_tensor(idx):
         idx = idx.tolist()
     
-    img_name = os.path.join(self.root_dir, self.set_train.iloc[idx, 0])
-    image = io.imread(img_name)
+    img_id = self.set_data.iloc[idx,0]
+    image = io.imread(self.root_dir + self.set_data.iloc[idx, 0])
+    #
+    image = cv2.resize(image, (int(2100/2),int(1400/2)))
+    #
+    image = torch.from_numpy(image)
+    image = image.permute(2,0,1)
+
+    mask = []
+    idex = 1
+    for i in range(4):
+      mk= rle_to_mask(self.set_data.iloc[idx, idex])
+      #
+      mk = cv2.resize(mk, (int(2100/2),int(1400/2)))
+      #
+      mk = torch.from_numpy(mk)
+      mask.append(mk)
+      idex += 1
+    mask = torch.stack(mask)
     
+    sample = {'image':image,'mask':mask}
+    return sample

5215887bfe847a1d1ac47b14871e6776af926727
Camila Ramirez <mcamilaramirez@MacBook-Pro-de-Maria-3.local>
2020-01-07 12:45:41 -0500

    Se actualiza función con rle_to_mask

--- utils/get_mask.py
+++ utils/get_mask.py
@@ -11,4 +11,17 @@ def get_mask(image_id,data_frame):
       mk.append(t)
     mask = np.array(mk).reshape(-1,2)
     index += 1
-  return mask
+  a = np.where(mask == 'Fish')
+  a=mask[a[0],0]
+  m_fish = rle_to_mask(a[0])
+  a = np.where(mask == 'Flower')
+  a=mask[a[0],0]
+  m_flower = rle_to_mask(a[0])
+  a = np.where(mask == 'Gravel')
+  a=mask[a[0],0]
+  m_gravel = rle_to_mask(a[0])
+  a = np.where(mask == 'Sugar')
+  a=mask[a[0],0]
+  m_sugar = rle_to_mask(a[0])
+  masks = np.array([m_fish, m_flower,m_gravel,m_sugar])
+  return masks

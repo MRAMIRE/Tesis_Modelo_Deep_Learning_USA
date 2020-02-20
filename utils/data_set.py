2fe4ab06a39719545c19b7664f8ef632d3e3f86c
Camila Ramirez <mcamilaramirez@MacBook-Pro-de-Maria-3.local>
2020-02-20 16:23:53 -0500

    Cambio para sumition

--- utils/data_set.py
+++ utils/data_set.py
@@ -6,10 +6,11 @@ from PIL import Image
 from .rle_to_mask import *
 
 class CloudDataSet(Dataset):
-  def __init__(self, csv_file, root_dir,transform=None):
+  def __init__(self, csv_file, root_dir,transform=None, prediction=False):
         self.set_data = pd.read_csv(csv_file)
         self.root_dir =  root_dir
         self.transform = transform
+        self.prediction = prediction
 
   def __len__(self):
     return len(self.set_data)
@@ -18,20 +19,30 @@ class CloudDataSet(Dataset):
     if torch.is_tensor(idx):
         idx = idx.tolist()
     
-    img_id = self.set_data.iloc[idx,0]
-    image = Image.open(self.root_dir + self.set_data.iloc[idx, 0])
-    mask = []
-    idex = 1
-    for i in range (4):
-      mk = rle_to_mask(self.set_data.iloc[idx, idex])
-      mk = PIL.Image.fromarray(mk)
-      mask.append(mk)
-      idex += 1
+    if prediction = True:
+        img_id = self.set_data.iloc[idx,0]
+        image = Image.open(self.root_dir + self.set_data.iloc[idx, 0])
+
+        if self.transform:
+            image = self.transform(image)
+        return image
     
-    if self.transform:
-      image = self.transform(image)
-      mask = [self.transform(mask[i]).squeeze() for i in range(4)]
-      mask = torch.stack(mask)
-    
-    sample = {'image':image,'mask':mask}
-    return sample
+    else:
+
+        img_id = self.set_data.iloc[idx,0]
+        image = Image.open(self.root_dir + self.set_data.iloc[idx, 0])
+        mask = []
+        idex = 1
+        
+        for i in range (4):
+            mk = rle_to_mask(self.set_data.iloc[idx, idex])
+            mk = PIL.Image.fromarray(mk)
+            mask.append(mk)
+            idex += 1
+        
+        if self.transform:
+            image = self.transform(image)
+            mask = [self.transform(mask[i]).squeeze() for i in range(4)]
+            mask = torch.stack(mask)
+            sample = {'image':image,'mask':mask}
+        return sample
